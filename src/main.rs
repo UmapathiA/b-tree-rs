97c8fb007467a74c039ae591c16bd7d6031a9efa
Umapathi Ankaiah <umapathiankaiah@mac.home>
2026-09-18 09:32:31 -0400

    Add additional comments explaining insertion

--- src/main.rs
+++ src/main.rs
@@ -43,26 +43,37 @@ fn insert(mut root: Node, v: u32) -> (Node, bool) {
     let i = root.index(v);
     let mut split_again = false;
     if root.children.is_empty() {
+        //When child overflows, split. We reached the tail of the
+        //recxursion
         root.keys.insert(i, v);
         if root.keys.len() > MAX_NODE_SIZE {
             root = split_node(root);
-            split_again = true;
+            split_again = true; //Let the caller know that there is a split
         }
     } else {
-        let next_node = root.children.remove(i);
+        let next_node = root.children.remove(i); //Remove the node next in line and
+        //take full ownership
         let (mut next_node, split) = insert(next_node, v);
         if split {
+            //In case of split, we expect a node with only one key. Deconstruct the node, extract
+            //the key and insert in parent
             root.keys.insert(i, next_node.keys.pop().unwrap());
+            //Get the grand-children node and insert them in parent relative to the index
+            //calculated
             let e_1 = next_node.children.pop().unwrap();
             let e_2 = next_node.children.pop().unwrap();
             root.children.insert(i, e_2);
             root.children.insert(i + 1, e_1);
             if root.keys.len() > MAX_NODE_SIZE {
+                //In case of overflow, split again and let the
+                //caller know
                 root = split_node(root);
                 split_again = true;
             }
         } else {
-            root.children.insert(i, next_node);
+            root.children.insert(i, next_node); //In case there is no split, insert
+            //the previously removed node in the same place. Since there is no new nodes, no need
+            //to further process the tree.
         }
     }
     (root, split_again)
